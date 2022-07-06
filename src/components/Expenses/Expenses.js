382a50d958cf1eed99e75677659fff21a3a50af1
Daniel Spindler <daniel.spindler@leebmann.de>
2022-07-06 16:43:51 +0200

    events, Data passing

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear]= useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+       setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className={"expenses"}>
+            <ExpensesFilter onFilterChange={filterChangeHandler} selected={filteredYear} />
             <ExpenseItem title={props.items[0].title}
                          amount={props.items[0].amount}
                          date={props.items[0].date}
