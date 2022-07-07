295cc436f226f02550bba27f62a239f2a7361dda
Daniel Spindler <daniel.spindler@leebmann.de>
2022-07-07 17:33:14 +0200

    conditional

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,15 +11,29 @@ function Expenses(props) {
         setFilteredYear(selectedYear);
     };
 
-    return (
-        <Card className={"expenses"}>
-            <ExpensesFilter onFilterChange={filterChangeHandler} selected={filteredYear}/>
-            {props.items.map((expense) => (
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = (
+            filteredExpenses.map((expense) => (
                 <ExpenseItem key={expense.id}
                              title={expense.title}
                              amount={expense.amount}
                              date={expense.date}/>
-            ))}
+            ))
+        );
+    }
+
+    return (
+        <Card className={"expenses"}>
+            <ExpensesFilter onFilterChange={filterChangeHandler} selected={filteredYear}/>
+            {expensesContent}
+
         </Card>
     );
 }
