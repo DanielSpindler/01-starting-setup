93abd7c24b500ad746f88320a3bafde1a2c878a8
Daniel Spindler <daniel.spindler@leebmann.de>
2022-07-07 18:46:51 +0200

    conditional return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import {useState} from "react";
 
 function Expenses(props) {
@@ -15,25 +16,10 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-
-    let expensesContent = <p>No expenses found.</p>;
-
-    if(filteredExpenses.length > 0) {
-        expensesContent = (
-            filteredExpenses.map((expense) => (
-                <ExpenseItem key={expense.id}
-                             title={expense.title}
-                             amount={expense.amount}
-                             date={expense.date}/>
-            ))
-        );
-    }
-
     return (
         <Card className={"expenses"}>
             <ExpensesFilter onFilterChange={filterChangeHandler} selected={filteredYear}/>
-            {expensesContent}
-
+            <ExpensesList items={filteredExpenses}/>
         </Card>
     );
 }
