0e59383b06eaaccde4dfd9b8a4df82c3af7ae019
Daniel Spindler <daniel.spindler@leebmann.de>
2022-07-08 10:33:41 +0200

    Chart added. Dynamic CSS .map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import {useState} from "react";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -19,6 +20,7 @@ function Expenses(props) {
     return (
         <Card className={"expenses"}>
             <ExpensesFilter onFilterChange={filterChangeHandler} selected={filteredYear}/>
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
         </Card>
     );
